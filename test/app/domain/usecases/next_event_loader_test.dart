e2f507a9686c12e3577f27358a3c5bb811f1faee
Bruno Noveli <brunimzuppwolf@gmail.com>
2025-01-06 21:36:20 -0300

    Changed NextEventLoader to use an interface of a repository

--- test/app/domain/usecases/next_event_loader_test.dart
+++ test/app/domain/usecases/next_event_loader_test.dart
@@ -1,11 +1,13 @@
+import 'dart:math';
+
 import 'package:flutter_test/flutter_test.dart';
 
 class NextEventLoader {
 
-  final LoadNextEventRepository _loadNextEventRepository;
+  final ILoadNextEventRepository _loadNextEventRepository;
 
   NextEventLoader({
-    required LoadNextEventRepository loadNextEventRepository,
+    required ILoadNextEventRepository loadNextEventRepository,
   }) : _loadNextEventRepository = loadNextEventRepository;
 
   Future<void> call({required String groupId}) async {
@@ -13,32 +15,51 @@ class NextEventLoader {
   }
 }
 
-class LoadNextEventRepository {
+/// Mock, Spy, Stub
+/// When I test both input and output it's a spy;
+/// When I am worried only about the input it's a mock;
+/// When I am worried only about the output it's a stub;
+class LoadNextEventMockRepository implements ILoadNextEventRepository {
 
-  var isCalled = false;
+  /// These properties make sense just in this case, when I make a Mock
+  /// There is no sense doing this in a real repository implementation.
+  /// But in the case of mocks it's acceptable to test things like if
+  /// the inputs (parameters) are correct.
+  String? groupId;
+  var callsCount = 0;
 
+  @override
   Future<void> loadNextEvent({required String groupId}) async {
-    isCalled = true;
+    this.groupId = groupId;
+    callsCount = 1;
   }
 }
 
+abstract interface class ILoadNextEventRepository {
+
+  Future<void> loadNextEvent({required String groupId});
+}
+
 void main() {
+
+  final groupId = Random().nextInt(3000).toString();
 
   test(
     "Should load event data from a repository",
     () async {
 
-      final repo = LoadNextEventRepository();
+      final repo = LoadNextEventMockRepository();
 
       final sut = NextEventLoader(
         loadNextEventRepository: repo,
       );
 
       await sut(
-        groupId: "",
+        groupId: groupId,
       );
 
-      expect(repo.isCalled, true);
+      expect(repo.groupId, groupId);
+      expect(repo.callsCount, 1);
     },
   );
 }
