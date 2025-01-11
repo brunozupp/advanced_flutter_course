3b90242337bdd528ce6bf90cb90ae7d0c5fcd0b2
Bruno Noveli <brunimzuppwolf@gmail.com>
2025-01-11 10:18:10 -0300

    refactor: moved load_next_event_api_repository_test to another directory and moved http_client_spy to a specific directory

---
+++ test/app/infra/repositories/api/clients/http_client_spy.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:http/http.dart';
+
+class HttpClientSpy implements Client {
+
+  String? method;
+  int callsCount = 0;
+  String? url;
+  Map<String,String>? headers;
+  String responseJson = "";
+  int statusCode = 200;
+
+  @override
+  void close() { }
+
+  @override
+  Future<Response> delete(Uri url, {Map<String, String>? headers, Object? body, Encoding? encoding}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Response> get(Uri url, {Map<String, String>? headers}) async {
+    method = "GET";
+    callsCount++;
+    this.url = url.toString();
+    this.headers = headers;
+
+    return Response(responseJson, statusCode);
+  }
+
+  @override
+  Future<Response> head(Uri url, {Map<String, String>? headers}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Response> patch(Uri url, {Map<String, String>? headers, Object? body, Encoding? encoding}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Response> post(Uri url, {Map<String, String>? headers, Object? body, Encoding? encoding}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Response> put(Uri url, {Map<String, String>? headers, Object? body, Encoding? encoding}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> read(Uri url, {Map<String, String>? headers}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Uint8List> readBytes(Uri url, {Map<String, String>? headers}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<StreamedResponse> send(BaseRequest request) {
+    throw UnimplementedError();
+  }
+}
--- test/app/infra/repositories/api/load_next_event_api_repository_test.dart
+++ test/app/infra/repositories/api/load_next_event_api_repository_test.dart
@@ -1,76 +1,11 @@
 import 'dart:convert';
 
-import 'dart:typed_data';
-
 import 'package:advanced_flutter_course/app/domain/entities/enums/domain_error.dart';
 import 'package:advanced_flutter_course/app/infra/repositories/api/load_next_event_api_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart';
 
-import '../../../helpers/fakes.dart';
-
-class HttpClientSpy implements Client {
-
-  String? method;
-  int callsCount = 0;
-  String? url;
-  Map<String,String>? headers;
-  String responseJson = "";
-  int statusCode = 200;
-
-  @override
-  void close() { }
-
-  @override
-  Future<Response> delete(Uri url, {Map<String, String>? headers, Object? body, Encoding? encoding}) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Response> get(Uri url, {Map<String, String>? headers}) async {
-    method = "GET";
-    callsCount++;
-    this.url = url.toString();
-    this.headers = headers;
-
-    return Response(responseJson, statusCode);
-  }
-
-  @override
-  Future<Response> head(Uri url, {Map<String, String>? headers}) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Response> patch(Uri url, {Map<String, String>? headers, Object? body, Encoding? encoding}) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Response> post(Uri url, {Map<String, String>? headers, Object? body, Encoding? encoding}) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Response> put(Uri url, {Map<String, String>? headers, Object? body, Encoding? encoding}) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<String> read(Uri url, {Map<String, String>? headers}) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Uint8List> readBytes(Uri url, {Map<String, String>? headers}) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<StreamedResponse> send(BaseRequest request) {
-    throw UnimplementedError();
-  }
-}
+import '../../../../helpers/fakes.dart';
+import 'clients/http_client_spy.dart';
 
 void main() {
 
