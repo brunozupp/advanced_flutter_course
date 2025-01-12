ecf1e86cd6cdaa84676fc697276b02811d8f82e0
Bruno Noveli <brunimzuppwolf@gmail.com>
2025-01-12 11:52:16 -0300

    test: ensure HttpClient request with correct url

--- test/app/infra/repositories/api/clients/http_client_test.dart
+++ test/app/infra/repositories/api/clients/http_client_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 
+import '../../../../../helpers/fakes.dart';
 import 'client_spy.dart';
 
 class HttpClient {
@@ -11,8 +12,10 @@ class HttpClient {
     required Client client,
   }) : _client = client;
 
-  Future<void> get() async {
-    await _client.get(Uri());
+  Future<void> get({
+    required String url,
+  }) async {
+    await _client.get(Uri.parse(url));
   }
 }
 
@@ -20,23 +23,48 @@ void main() {
 
   late ClientSpy client;
   late HttpClient sut;
+  late String url;
 
   setUp(() {
+
     client = ClientSpy();
 
     sut = HttpClient(
       client: client,
     );
+
+    url = anyString();
   });
 
-  test(
-    "Should request with correct method",
-    () async {
+  group(
+    "Tests from .get method",
+    () {
+      test(
+        "Should request with correct method",
+        () async {
 
-      await sut.get();
+          await sut.get(
+            url: url,
+          );
 
-      expect(client.method, "GET");
-      expect(client.callsCount, 1);
+          expect(client.method, "GET");
+          expect(client.callsCount, 1);
+        },
+      );
+
+      test(
+        "Should request with correct url",
+        () async {
+
+          await sut.get(
+            url: url,
+          );
+
+          expect(client.url, url);
+        },
+      );
     },
   );
+
+
 }
