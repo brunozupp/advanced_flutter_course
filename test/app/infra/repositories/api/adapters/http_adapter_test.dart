acf5563005c95d59cc896a7d252ddb0beccf75da
Bruno Noveli <brunimzuppwolf@gmail.com>
2025-01-14 08:58:10 -0300

    refactor: changed name from HttpClient to HttpAdapter | moved HttpAdapter to specific file

--- test/app/infra/repositories/api/adapters/http_adapter_test.dart
+++ test/app/infra/repositories/api/adapters/http_adapter_test.dart
@@ -1,134 +1,18 @@
 import 'dart:convert';
 
 import 'package:advanced_flutter_course/app/domain/entities/enums/domain_error.dart';
+import 'package:advanced_flutter_course/app/infra/repositories/api/adapters/http_adapter.dart';
 import 'package:advanced_flutter_course/app/infra/types/json_type.dart';
-import 'package:dartx/dartx.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart';
 
 import '../../../../../helpers/fakes.dart';
-import 'client_spy.dart';
+import '../clients/client_spy.dart';
 
-class HttpClient {
-
-  final Client _client;
-
-  HttpClient({
-    required Client client,
-  }) : _client = client;
-
-  Future<T?> get<T>({
-    required String url,
-    Map<String, String>? headers,
-    Map<String, String?>? params,
-    Map<String, String>? queryString,
-  }) async {
-
-    final uri = _buildUrl(
-      url: url,
-      params: params,
-      queryString: queryString,
-    );
-
-    final response = await _client.get(
-      uri,
-      headers: {
-        "content-type": "application/json",
-        "accept": "application/json",
-        if(headers != null) ...headers,
-      },
-    );
-
-    switch(response.statusCode) {
-      /// I don't need this line of code because I already treat
-      /// the cases where I don't have any body in my response. And
-      /// this rule is the case of 204 (NoContent), that is a positive
-      /// response that doesn't have any body.
-      /// So the logic will be lead by the standards of the backend.
-      /// To this scenario I will let this line here.
-      case 204:
-        return null;
-      case 400:
-      case 403:
-      case 404:
-      case 500:
-        throw DomainError.unexpected;
-      case 401:
-        throw DomainError.sessionExpired;
-    }
-
-    /// To the cases where response from 200 is null
-    if(response.body.isEmpty) {
-      return null;
-    }
-
-    final responseDecode = jsonDecode(response.body);
-
-    /// I need to do this because Dart is not good when it comes to
-    /// parse values, so I need to specify the type of my list as
-    /// I put the type of this method where I am executed it.
-    if(T == JsonList) {
-      return responseDecode.map<Json>((e) => e as Json).toList();
-    }
-
-    return responseDecode;
-  }
-
-  Uri _buildUrl({
-    required String url,
-    Map<String, String?>? params,
-    Map<String, String>? queryString,
-  }) {
-
-    var urlToFormat = url;
-
-    urlToFormat = params?.keys
-      .fold(
-        urlToFormat,
-        (result, key) =>
-            result.replaceFirst(":$key", params[key] ?? ""))
-      .removeSuffix("/") ??
-    urlToFormat;
-
-    urlToFormat = queryString?.keys
-      .fold(
-        "$urlToFormat?",
-        (result, key) => "$result$key=${queryString[key]}&")
-      .removeSuffix("&") ??
-    urlToFormat;
-
-    // params?.forEach((key, value) {
-
-    //   if(value == null) {
-    //     urlToFormat = urlToFormat.replaceFirst("/:$key", "");
-    //   } else {
-    //     urlToFormat = urlToFormat.replaceFirst(":$key", value);
-    //   }
-    // });
-
-    // final queryStringList = queryString?.entries.toList() ?? [];
-
-    // for (var i = 0; i < queryStringList.length; i++) {
-
-    //   if(i == 0) {
-    //     urlToFormat += "?";
-    //   }
-
-    //   urlToFormat += "${queryStringList[i].key}=${queryStringList[i].value}";
-
-    //   if(i < queryStringList.length - 1) {
-    //     urlToFormat += "&";
-    //   }
-    // }
-
-    return Uri.parse(urlToFormat);
-  }
-}
 
 void main() {
 
   late ClientSpy client;
-  late HttpClient sut;
+  late HttpAdapter sut;
   late String url;
 
   setUp(() {
@@ -136,7 +20,7 @@ void main() {
     client = ClientSpy();
     client.responseJson = jsonEncode(mapNextEvent);
 
-    sut = HttpClient(
+    sut = HttpAdapter(
       client: client,
     );
 
