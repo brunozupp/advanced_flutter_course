78e156969fdd9af8497b540f4a5fb8a5c40764f0
Bruno Noveli <brunimzuppwolf@gmail.com>
2025-01-14 08:43:53 -0300

    test: ensure HttpClient returns null on 204

--- test/app/infra/repositories/api/clients/http_client_test.dart
+++ test/app/infra/repositories/api/clients/http_client_test.dart
@@ -40,6 +40,14 @@ class HttpClient {
     );
 
     switch(response.statusCode) {
+      /// I don't need this line of code because I already treat
+      /// the cases where I don't have any body in my response. And
+      /// this rule is the case of 204 (NoContent), that is a positive
+      /// response that doesn't have any body.
+      /// So the logic will be lead by the standards of the backend.
+      /// To this scenario I will let this line here.
+      case 204:
+        return null;
       case 400:
       case 403:
       case 404:
@@ -49,12 +57,16 @@ class HttpClient {
         throw DomainError.sessionExpired;
     }
 
+    /// To the cases where response from 200 is null
     if(response.body.isEmpty) {
       return null;
     }
 
     final responseDecode = jsonDecode(response.body);
 
+    /// I need to do this because Dart is not good when it comes to
+    /// parse values, so I need to specify the type of my list as
+    /// I put the type of this method where I am executed it.
     if(T == JsonList) {
       return responseDecode.map<Json>((e) => e as Json).toList();
     }
@@ -428,6 +440,19 @@ void main() {
 
           client.responseJson = "";
           client.statusCode = 200;
+
+          final data = await sut.get(url: url);
+
+          expect(data, isNull);
+        },
+      );
+
+      test(
+        "Should return null on 204",
+        () async {
+
+          client.statusCode = 204;
+          client.responseJson = "";
 
           final data = await sut.get(url: url);
 
