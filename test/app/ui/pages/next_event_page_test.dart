fbb697a5a586decf6a7c9fb62c4c9f6c7f173f5a
Bruno Noveli <brunimzuppwolf@gmail.com>
2025-01-16 09:45:19 -0300

    test: ensure NextEventPage loads next event data on page init

---
+++ test/app/ui/pages/next_event_page_test.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../helpers/fakes.dart';
+
+final class NextEventPage extends StatefulWidget {
+
+  final NextEventPresenter presenter;
+  final String groupId;
+
+  const NextEventPage({
+    super.key,
+    required this.presenter,
+    required this.groupId,
+  });
+
+  @override
+  State<NextEventPage> createState() => _NextEventPageState();
+}
+
+class _NextEventPageState extends State<NextEventPage> {
+
+  NextEventPresenter get presenter => widget.presenter;
+
+  @override
+  void initState() {
+    super.initState();
+
+    presenter.loadNextEvent(groupId: widget.groupId);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
+
+abstract interface class NextEventPresenter {
+
+  void loadNextEvent({
+    required String groupId,
+  });
+}
+
+final class NextEventPresenterSpy implements NextEventPresenter {
+
+  int loadCallsCount = 0;
+  String? groupId;
+
+  @override
+  void loadNextEvent({required String groupId}) {
+    loadCallsCount++;
+    this.groupId = groupId;
+  }
+
+}
+
+void main() {
+
+  testWidgets(
+    "Should load event data on page init",
+    (WidgetTester tester) async {
+
+      final presenter = NextEventPresenterSpy();
+      final groupId = anyString();
+
+      /// As I am testing a widget from Material Design it needs to be
+      /// inside a MaterialApp
+      final sut = MaterialApp(
+        home: NextEventPage(
+          presenter: presenter,
+          groupId: groupId,
+        ),
+      );
+
+      /// To test a component I need to create it virtually, so it can emulate
+      /// this component to be tested virtually.
+      /// And to do this I need to use the method .pump
+
+      await tester.pumpWidget(sut);
+
+      expect(presenter.loadCallsCount, 1);
+      expect(presenter.groupId, groupId);
+    },
+  );
+}
