7ab14ae726c6930cdacafc24c189ba9918459483
Bruno Noveli <brunimzuppwolf@gmail.com>
2025-01-16 10:54:12 -0300

    test: ensure NextEventPage hides spinner on load success

--- test/app/ui/pages/next_event_page_test.dart
+++ test/app/ui/pages/next_event_page_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:rxdart/subjects.dart';
 
 import '../../../helpers/fakes.dart';
 
@@ -31,13 +32,24 @@ class _NextEventPageState extends State<NextEventPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: CircularProgressIndicator(),
+    return Scaffold(
+      body: StreamBuilder(
+        stream: presenter.nextEventStream,
+        builder: (context, snapshot) {
+          if(snapshot.connectionState != ConnectionState.active) {
+            return const CircularProgressIndicator();
+          }
+
+          return const SizedBox.shrink();
+        },
+      ),
     );
   }
 }
 
 abstract interface class NextEventPresenter {
+
+  Stream get nextEventStream;
 
   void loadNextEvent({
     required String groupId,
@@ -48,6 +60,14 @@ final class NextEventPresenterSpy implements NextEventPresenter {
 
   int loadCallsCount = 0;
   String? groupId;
+  var nextEventSubject = BehaviorSubject();
+
+  @override
+  Stream get nextEventStream => nextEventSubject.stream;
+
+  void emitNextEvent() {
+    nextEventSubject.add("");
+  }
 
   @override
   void loadNextEvent({required String groupId}) {
@@ -84,6 +104,30 @@ void main() {
       await tester.pumpWidget(sut);
 
       expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    "Should hide spinner on load success",
+    (WidgetTester tester) async {
+
+      /// When I execute this method I guarantee that I build
+      /// this component virtually and it is with that initial image
+      /// that it asks to render
+      await tester.pumpWidget(sut);
+
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+      presenter.emitNextEvent();
+
+      /// So everything that is async (schedule to the future) I need to force my screen to rerender
+      /// virtually because has things to happen (new ui). So to do this
+      /// update I need to use the .pump method. This will execute the next loop from the event
+      /// and will update the screen
+
+      await tester.pump();
+
+      expect(find.byType(CircularProgressIndicator), findsNothing);
     },
   );
 }
