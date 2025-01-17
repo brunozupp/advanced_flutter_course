48356bb2f9e31b8087ff997943a61fe6ccde4f01
Bruno Noveli <brunimzuppwolf@gmail.com>
2025-01-17 15:17:27 -0300

    test: ensure PlayerPhoto shows photo when there is photo parameter passed

--- test/app/ui/widgets/player_photo_test.dart
+++ test/app/ui/widgets/player_photo_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:network_image_mock/network_image_mock.dart';
 
 /// To widgets like this that are used in more than one page/module
 /// it is not recommended to put the viewmodel as a paramters, because
@@ -21,12 +22,19 @@ class PlayerPhoto extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CircleAvatar(
-      child: Text("BN"),
+      foregroundImage: photo != null
+        ? NetworkImage(photo!)
+        : null,
+      child: photo == null
+        ? Text(initials)
+        : null,
     );
   }
 }
 
 void main() {
+
+  const urlImage = "https://conteudo.imguol.com.br/c/entretenimento/af/2024/02/21/cena-de-naruto-shippuden-1708550179304_v2_900x506.jpg";
 
   testWidgets(
     "should present initials when there is no photo",
@@ -42,6 +50,31 @@ void main() {
       await tester.pumpWidget(sut);
 
       expect(find.text("BN"), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    "should hide initials when there is photo",
+    (WidgetTester tester) async {
+
+      /// When I have to deal with network calls to get images I need
+      /// to use this mockNetworkImagesFor from the plugin network_image_mock
+      /// If I don't use it I will get an error when running the test
+      mockNetworkImagesFor(() async {
+        const sut = MaterialApp(
+          home: PlayerPhoto(
+            initials: "BN",
+            photo: "https://any-url.com",
+          ),
+        );
+
+        await tester.pumpWidget(sut);
+
+        final circleAvatar = tester.firstWidget<CircleAvatar>(find.byType(CircleAvatar));
+
+        expect(find.text("BN"), findsNothing);
+        expect(circleAvatar.foregroundImage, isNotNull);
+      });
     },
   );
 }
