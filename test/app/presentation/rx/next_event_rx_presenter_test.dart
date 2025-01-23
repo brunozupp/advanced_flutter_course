15f337888e59f1c598397e952c7af6dc74254046
Bruno Noveli <brunimzuppwolf@gmail.com>
2025-01-22 21:36:33 -0300

    doc: explanation about the dependecy between presenter and usecase

--- test/app/presentation/rx/next_event_rx_presenter_test.dart
+++ test/app/presentation/rx/next_event_rx_presenter_test.dart
@@ -2,6 +2,18 @@ import 'package:flutter_test/flutter_test.dart';
 
 import '../../../helpers/fakes.dart';
 
+/// There is no need to have an abstraction from the Usecase to be passed
+/// to the Presenter as it is only one method. But to not let the Presenter
+/// tyed with the concrete class Usecase I can just pass the signature from
+/// the call method inside that usecase. Doing this I don't couple the
+/// concrete class inside the Presenter and I can change its implementation
+/// by just calling another usecase with the same method's signature. And
+/// this is good to test too as I don't need to depend on a concrete class.
+/// Summarizing I will depend on a function and not on a concrete class.
+///
+/// But the usecase is a layer that is abstracted enough. It's rare the
+/// time I will have 2 implementations from a usecase. That's why I
+/// don't create an interface to the usecase.
 final class NextEventRxPresenter {
 
   final Future<void> Function({
