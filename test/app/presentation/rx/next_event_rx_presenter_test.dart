f4ce6a0c449518d57dedb8aed6c7bd96846936ec
Bruno Noveli <brunimzuppwolf@gmail.com>
2025-01-24 08:29:35 -0300

    test: ensure NextEventRxPresenter emits correct events on reload with error

--- test/app/presentation/rx/next_event_rx_presenter_test.dart
+++ test/app/presentation/rx/next_event_rx_presenter_test.dart
@@ -1,4 +1,7 @@
+@Timeout(Duration(seconds: 1))
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:rxdart/subjects.dart';
 
 import '../../../helpers/fakes.dart';
 
@@ -20,14 +23,28 @@ final class NextEventRxPresenter {
     required String groupId,
   }) nextEventLoader;
 
-  const NextEventRxPresenter({
+  NextEventRxPresenter({
     required this.nextEventLoader,
   });
 
+  final _nextEventSubject = BehaviorSubject();
+  final _isBusyStream = BehaviorSubject<bool>();
+
+  Stream get nextEventStream => _nextEventSubject.stream;
+  Stream<bool> get isBusyStream => _isBusyStream.stream;
+
   Future<void> loadNextEvent({
     required String groupId,
+    bool isReload = false,
   }) async {
-    await nextEventLoader(groupId: groupId);
+
+    try {
+      _isBusyStream.add(true);
+      await nextEventLoader(groupId: groupId);
+    } catch (e) {
+      _nextEventSubject.addError(e);
+      _isBusyStream.add(false);
+    }
   }
 }
 
@@ -35,12 +52,17 @@ final class NextEventLoaderUseCaseSpy {
 
   int callsCount = 0;
   String? groupId;
+  Error? error;
 
   Future<void> call({
     required String groupId,
   }) async {
     this.groupId = groupId;
     callsCount++;
+
+    if(error != null) {
+      throw error!;
+    }
   }
 }
 
@@ -69,6 +91,59 @@ void main() {
 
       expect(nextEventLoader.callsCount, 1);
       expect(nextEventLoader.groupId, groupId);
+    },
+  );
+
+  test(
+    "Should emit correct events on reload with error",
+    () async {
+
+      /// Arrange
+      nextEventLoader.error = Error();
+
+      /// See the explanation to this block of code in the comment below
+      /// It's the same as the block below (sugar sintax)
+      // sut.nextEventStream.listen(
+      //   null,
+      //   onError: (error) {
+      //     expect(error, nextEventLoader.error);
+      //   }
+      // );
+
+      /// It works the same as the block of code above. This is just
+      /// a sugar sintax to test streams.
+      expectLater(
+        sut.nextEventStream,
+        emitsError(nextEventLoader.error),
+      );
+
+      /// I could have had two equal expectLater, but each one with
+      /// emits(true/false), this would work two. But I can use the
+      /// emitsInOrder to guarantee that it test in the order I passed
+      /// in the array
+      expectLater(
+        sut.isBusyStream,
+        emitsInOrder([true, false]),
+      );
+
+      /// When my tests in a stream give me an error it will not fail
+      /// on time. It will wait 30 seconds. To this, I can change the
+      /// timeout passing a new one in the .test method.
+      /// Another way of doing this that is more performatic is applying
+      /// this change in the top of this file using @Timeout
+
+      /// Act
+      await sut.loadNextEvent(
+        groupId: groupId,
+        isReload: true,
+      );
+
+      /// IMPORTANT: Test STREAM
+      /// To test a Stream I need to listen to it before the
+      /// 'act' part is executed. My Asset (the third part
+      /// of the TDD) stays inside the Stream.listen. Because
+      /// inside it that I will have access to the values
+      /// and so I will put the expects there.
     },
   );
 }
