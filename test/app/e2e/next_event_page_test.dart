a548d679f0ee1d8d6da7be0cb3f28b5e52431a05
Bruno Noveli <brunimzuppwolf@gmail.com>
2025-03-12 16:30:53 -0300

    test: ensure page show API data

--- test/app/e2e/next_event_page_test.dart
+++ test/app/e2e/next_event_page_test.dart
@@ -1,8 +1,14 @@
 import 'dart:convert';
 
+import 'package:advanced_flutter_course/app/core/factories/common_factory.dart';
 import 'package:advanced_flutter_course/app/core/factories/mapper_factory.dart';
+import 'package:advanced_flutter_course/app/core/factories/repository_factory.dart';
+import 'package:advanced_flutter_course/app/infra/mappers/next_event_mapper.dart';
 import 'package:advanced_flutter_course/app/infra/repositories/api/adapters/http_adapter.dart';
 import 'package:advanced_flutter_course/app/infra/repositories/api/load_next_event_api_repository.dart';
+import 'package:advanced_flutter_course/app/infra/repositories/api_with_cache/load_next_event_api_with_cache_fallback_repository.dart';
+import 'package:advanced_flutter_course/app/infra/repositories/cache/adapters/cache_manager_adapter.dart';
+import 'package:advanced_flutter_course/app/infra/repositories/cache/load_next_event_cache_repository.dart';
 import 'package:advanced_flutter_course/app/presentation/rx/next_event_rx_presenter.dart';
 import 'package:advanced_flutter_course/app/ui/pages/next_event/next_event_page.dart';
 import 'package:flutter/material.dart';
@@ -10,31 +16,74 @@ import 'package:flutter_test/flutter_test.dart';
 
 import '../../mocks/fakes.dart';
 import '../infra/repositories/api/mocks/client_spy.dart';
+import '../infra/repositories/cache/mocks/cache_manager_spy.dart';
 
 void main() {
 
+  late final String responseMocked;
+  late ClientSpy client;
+  late HttpAdapter httpAdapter;
+  late LoadNextEventApiRepository apiRepository;
+  late LoadNextEventCacheRepository cacheRepository;
+  late CacheManagerAdapter cacheClient;
+  late CacheManagerSpy cacheManager;
+  late LoadNextEventApiWithCacheFallbackRepository compositeRepository;
+  late NextEventRxPresenter presenter;
+  late String cacheKey;
+  late NextEventMapper mapper;
+  late Widget sut;
+
+  setUpAll(() {
+    responseMocked = _getResponseJson();
+  });
+
+  setUp(() {
+
+    client = ClientSpy();
+
+    httpAdapter = HttpAdapter(client: client);
+
+    mapper = MapperFactory.makeNextEventMapper();
+
+    cacheKey = anyString();
+
+    apiRepository = LoadNextEventApiRepository(
+      httpClient: httpAdapter,
+      url: anyString(),
+      mapper: mapper,
+    );
+
+    cacheManager = CacheManagerSpy();
+    cacheClient = CacheManagerAdapter(client: cacheManager);
+    cacheRepository = LoadNextEventCacheRepository(
+      cacheClient: cacheClient,
+      key: cacheKey,
+      mapper: mapper,
+    );
+
+    compositeRepository = LoadNextEventApiWithCacheFallbackRepository(
+      cacheClient: cacheClient,
+      key: cacheKey,
+      loadNextEventApi: apiRepository.loadNextEvent,
+      loadNextEventCache: cacheRepository.loadNextEvent,
+      mapper: mapper,
+    );
+
+    presenter = NextEventRxPresenter(nextEventLoader: compositeRepository.loadNextEvent);
+
+    sut = MaterialApp(
+      home: NextEventPage(
+        presenter: presenter,
+        groupId: anyString(),
+      ),
+    );
+  });
+
   testWidgets(
-    "Should present next event page",
+    "Should present api data",
     (WidgetTester tester) async {
 
-      final client = ClientSpy();
-
-      client.responseJson = _getResponseJson();
-
-      final httpClient = HttpAdapter(client: client);
-      final repository = LoadNextEventApiRepository(
-        httpClient: httpClient,
-        url: anyString(),
-        mapper: MapperFactory.makeNextEventMapper(),
-      );
-      final presenter = NextEventRxPresenter(nextEventLoader: repository.loadNextEvent);
-
-      final sut = MaterialApp(
-        home: NextEventPage(
-          presenter: presenter,
-          groupId: anyString(),
-        ),
-      );
+      client.responseJson = responseMocked;
 
       await tester.pumpWidget(sut);
       await tester.pump();
@@ -60,71 +109,71 @@ void main() {
   );
 }
 
-  String _getResponseJson() {
-    return jsonEncode({
-      "id": "1",
-      "groupName": "Pelada Chega+",
-      "date": "2024-01-11T11:10:00.000Z",
-      "players": [
-        {
-          "id": "1",
-          "name": "Cristiano Ronaldo",
-          "position": "forward",
-          "isConfirmed": true,
-          "confirmationDate": "2024-01-10T11:07:00.000Z"
-        },
-        {
-          "id": "2",
-          "name": "Lionel Messi",
-          "position": "midfielder",
-          "isConfirmed": true,
-          "confirmationDate": "2024-01-10T11:08:00.000Z"
-        },
-        {
-          "id": "3",
-          "name": "Dida",
-          "position": "goalkeeper",
-          "isConfirmed": true,
-          "confirmationDate": "2024-01-10T09:10:00.000Z"
-        },
-        {
-          "id": "4",
-          "name": "Romario",
-          "position": "forward",
-          "isConfirmed": true,
-          "confirmationDate": "2024-01-10T11:10:00.000Z"
-        },
-        {
-          "id": "5",
-          "name": "Claudio Gamarra",
-          "position": "defender",
-          "isConfirmed": false,
-          "confirmationDate": "2024-01-10T13:10:00.000Z"
-        },
-        {
-          "id": "6",
-          "name": "Diego Forlan",
-          "position": "defender",
-          "isConfirmed": false,
-          "confirmationDate": "2024-01-10T14:10:00.000Z"
-        },
-        {
-          "id": "7",
-          "name": "Zé Ninguém",
-          "isConfirmed": false
-        },
-        {
-          "id": "8",
-          "name": "Rodrigo Manguinho",
-          "isConfirmed": false
-        },
-        {
-          "id": "9",
-          "name": "Claudio Taffarel",
-          "position": "goalkeeper",
-          "isConfirmed": true,
-          "confirmationDate": "2024-01-10T09:15:00.000Z"
-        },
-      ],
-    });
-  }
+String _getResponseJson() {
+  return jsonEncode({
+    "id": "1",
+    "groupName": "Pelada Chega+",
+    "date": "2024-01-11T11:10:00.000Z",
+    "players": [
+      {
+        "id": "1",
+        "name": "Cristiano Ronaldo",
+        "position": "forward",
+        "isConfirmed": true,
+        "confirmationDate": "2024-01-10T11:07:00.000Z"
+      },
+      {
+        "id": "2",
+        "name": "Lionel Messi",
+        "position": "midfielder",
+        "isConfirmed": true,
+        "confirmationDate": "2024-01-10T11:08:00.000Z"
+      },
+      {
+        "id": "3",
+        "name": "Dida",
+        "position": "goalkeeper",
+        "isConfirmed": true,
+        "confirmationDate": "2024-01-10T09:10:00.000Z"
+      },
+      {
+        "id": "4",
+        "name": "Romario",
+        "position": "forward",
+        "isConfirmed": true,
+        "confirmationDate": "2024-01-10T11:10:00.000Z"
+      },
+      {
+        "id": "5",
+        "name": "Claudio Gamarra",
+        "position": "defender",
+        "isConfirmed": false,
+        "confirmationDate": "2024-01-10T13:10:00.000Z"
+      },
+      {
+        "id": "6",
+        "name": "Diego Forlan",
+        "position": "defender",
+        "isConfirmed": false,
+        "confirmationDate": "2024-01-10T14:10:00.000Z"
+      },
+      {
+        "id": "7",
+        "name": "Zé Ninguém",
+        "isConfirmed": false
+      },
+      {
+        "id": "8",
+        "name": "Rodrigo Manguinho",
+        "isConfirmed": false
+      },
+      {
+        "id": "9",
+        "name": "Claudio Taffarel",
+        "position": "goalkeeper",
+        "isConfirmed": true,
+        "confirmationDate": "2024-01-10T09:15:00.000Z"
+      },
+    ],
+  });
+}
