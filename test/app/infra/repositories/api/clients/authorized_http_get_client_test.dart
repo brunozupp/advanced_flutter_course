790441f5d7f664eef619f265c103d65d95ac82af
Bruno Noveli <brunimzuppwolf@gmail.com>
2025-03-16 17:23:05 -0300

    test: ensure AuthorizedHttpGetClient calls HttpClient with correct input

--- test/app/infra/repositories/api/clients/authorized_http_get_client_test.dart
+++ test/app/infra/repositories/api/clients/authorized_http_get_client_test.dart
@@ -1,44 +1,79 @@
+import 'package:advanced_flutter_course/app/infra/repositories/api/clients/http_get_client.dart';
 import 'package:advanced_flutter_course/app/infra/repositories/cache/clients/cache_get_client.dart';
+import 'package:advanced_flutter_course/app/infra/types/json_type.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../../../../../mocks/fakes.dart';
 import '../../cache/mocks/cache_get_client_spy.dart';
+import '../mocks/http_get_client_spy.dart';
 
 final class AuthorizedHttpGetClient {
 
   final CacheGetClient _cacheClient;
+  final HttpGetClient _httpClient;
 
   const AuthorizedHttpGetClient({
     required CacheGetClient cacheClient,
-  }) : _cacheClient = cacheClient;
+    required HttpGetClient httpClient,
+  }) : _cacheClient = cacheClient, _httpClient = httpClient;
 
-  Future<void> get() async {
+  Future<void> get({
+    required String url,
+    Json? params,
+    Json? queryString,
+  }) async {
     await _cacheClient.get(key: 'current_user');
+    await _httpClient.get(
+      url: url,
+      params: params,
+      queryString: queryString,
+    );
   }
 }
 
 void main() {
 
-  CacheGetClientSpy cacheClient;
-  AuthorizedHttpGetClient sut;
+  late CacheGetClientSpy cacheClient;
+  late HttpGetClientSpy httpClient;
+  late AuthorizedHttpGetClient sut;
+  late String url;
+  late Json params;
+  late Json queryString;
 
   setUp(() {
     cacheClient = CacheGetClientSpy();
+    httpClient = HttpGetClientSpy();
     sut = AuthorizedHttpGetClient(
       cacheClient: cacheClient,
+      httpClient: httpClient,
     );
+    url = anyString();
+    params = anyJson();
+    queryString = anyJson();
   });
 
   test(
     "Should call CacheClient with correct input",
     () async {
 
-      cacheClient = CacheGetClientSpy();
-      sut = AuthorizedHttpGetClient(
-        cacheClient: cacheClient,
-      );
-      await sut.get();
+      await sut.get(url: url);
       expect(cacheClient.callsCount, 1);
       expect(cacheClient.key, 'current_user');
+    },
+  );
+
+  test(
+    "Should call HttpClient with correct input",
+    () async {
+      await sut.get(
+        url: url,
+        params: params,
+        queryString: queryString,
+      );
+      expect(httpClient.callsCount, 1);
+      expect(httpClient.url, url);
+      expect(httpClient.params, params);
+      expect(httpClient.queryString, queryString);
     },
   );
 }
